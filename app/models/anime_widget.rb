6dc6fed449dfb5645d4bb12d083f1d3cadbe43b5
Flinch <abdulmalikibrahim072@gmail.com>
2020-12-01 11:31:28 -0500

    deleted anime_model

--- app/models/anime_widget.rb
+++ app/models/anime_widget.rb
@@ -1,14 +1,14 @@
 class AnimeWidget < ApplicationRecord
 end
 
-class Anime 
-		attr_accessor :title, :id, :synopsis, :image, :rating
+# class Anime 
+# 		attr_accessor :title, :id, :synopsis, :image, :rating
 		
-		def initialize (title="NASS", id=0, synopsis="NA", image="NA", rating="NA")
-	  		@title = title
-	  		@id = id
-	  		@synopsis = synopsis
-	  		@image = image
-	  		@rating = rating
-		end
-	end
+# 		def initialize (title="NASS", id=0, synopsis="NA", image="NA", rating="NA")
+# 	  		@title = title
+# 	  		@id = id
+# 	  		@synopsis = synopsis
+# 	  		@image = image
+# 	  		@rating = rating
+# 		end
+# 	end
